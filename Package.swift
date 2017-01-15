86632658fffb77db7f59eef08852b9624c807531
Max Howell <mxcl@me.com>
2017-01-15 17:58:44 -0500

    Fix `Package.swift`

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ let package = Package(
     name: "PMKAlamofire",
     dependencies: [
         .Package(url: "https://github.com/mxcl/PromiseKit.git", majorVersion: 4),
-        .Package(url: "https://github.com/Alamofire/Alamofire.git", majorVersion: 3)
-    ]
+        .Package(url: "https://github.com/Alamofire/Alamofire.git", majorVersion: 4)
+    ],
+    exclude: ["Tests"]
 )
