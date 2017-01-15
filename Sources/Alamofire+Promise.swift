e1d8cc6913efc049676be5efafc5572b19d2b2e5
Max Howell <mxcl@me.com>
2017-01-15 17:58:35 -0500

    Add `responseJsonDictionary`

--- Sources/Alamofire+Promise.swift
+++ Sources/Alamofire+Promise.swift
@@ -71,6 +71,24 @@ extension Alamofire.DataRequest {
         }
     }
 
+    public func responseJsonDictionary(options: JSONSerialization.ReadingOptions = .allowFragments) -> Promise<[String: Any]> {
+        return Promise { fulfill, reject in
+            responseJSON(queue: nil, options: options, completionHandler: { response in
+                switch response.result {
+                case .success(let value):
+                    if let value = value as? [String: Any] {
+                        fulfill(value)
+                    } else {
+                        reject(JSONError.unexpectedRootNode(value))
+                    }
+                case .failure(let error):
+                    reject(error)
+                }
+            })
+        }
+
+    }
+
     /// Adds a handler to be called once the request has finished.
     public func responsePropertyList(options: PropertyListSerialization.ReadOptions = PropertyListSerialization.ReadOptions()) -> Promise<Any> {
         return Promise { fulfill, reject in
