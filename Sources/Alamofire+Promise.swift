e6de8a3595c5d3a291b397f8af9c61a460efee39
Max Howell <mxcl@me.com>
2017-01-23 15:22:06 -0500

    `responseJSON(with: .response)`
    Fixes #5

--- Sources/Alamofire+Promise.swift
+++ Sources/Alamofire+Promise.swift
@@ -72,6 +72,23 @@ extension Alamofire.DataRequest {
         }
     }
 
+    /// Adds a handler to be called once the request has finished. Provides access to the detailed response object.
+    ///    request.responseJSON(with: .response).then { json, response in }
+    public func responseJSON(with: PMKAlamofireOptions, options: JSONSerialization.ReadingOptions = .allowFragments) -> Promise<(Any, PMKDataResponse)> {
+        return Promise { fulfill, reject in
+            responseJSON(queue: nil, options: options, completionHandler: { response in
+                switch response.result {
+                case .success(let value):
+                    fulfill(value, PMKDataResponse(response))
+                case .failure(let error):
+                    reject(error)
+                }
+            })
+        }
+    }
+
+
+    /// Adds a handler to be called once the request has finished and the resulting JSON is rooted at a dictionary.
     public func responseJsonDictionary(options: JSONSerialization.ReadingOptions = .allowFragments) -> Promise<[String: Any]> {
         return Promise { fulfill, reject in
             responseJSON(queue: nil, options: options, completionHandler: { response in
@@ -119,4 +136,32 @@ extension Alamofire.DownloadRequest {
             }
         }
     }
+}
+
+
+
+public enum PMKAlamofireOptions {
+    case response
+}
+
+
+public struct PMKDataResponse {
+    fileprivate init(_ rawrsp: Alamofire.DataResponse<Any>) {
+        request = rawrsp.request
+        response = rawrsp.response
+        data = rawrsp.data
+        timeline = rawrsp.timeline
+    }
+
+    /// The URL request sent to the server.
+    public let request: URLRequest?
+
+    /// The server's response to the URL request.
+    public let response: HTTPURLResponse?
+
+    /// The data returned by the server.
+    public let data: Data?
+
+    /// The timeline of the complete lifecycle of the request.
+    public let timeline: Timeline
 }
