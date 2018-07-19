e921fa90d64f675b59a20bb44eea84b79a03f899
Doug <dougstein@gmail.com>
2018-07-18 17:44:51 -0700

    'Cancel' for PromiseKit

--- Tests/TestAlamofire.swift
+++ Tests/TestAlamofire.swift
@@ -1,3 +1,4 @@
+import PMKCancel
 import PMKAlamofire
 import OHHTTPStubs
 import PromiseKit
@@ -71,4 +72,76 @@ class AlamofireTests: XCTestCase {
         
     }
 #endif
+}
+
+//////////////////////////////////////////////////////////// Cancellation
+
+extension AlamofireTests {
+    func testCancel() {
+        let json: NSDictionary = ["key1": "value1", "key2": ["value2A", "value2B"]]
+        
+        OHHTTPStubs.stubRequests(passingTest: { $0.url!.host == "example.com" }) { _ in
+            return OHHTTPStubsResponse(jsonObject: json, statusCode: 200, headers: nil)
+        }
+        
+        let ex = expectation(description: "")
+        
+        firstly {
+            Alamofire.request("http://example.com", method: .get).responseJSONCC()
+        }.done { _ in
+            XCTFail("failed to cancel request")
+        }.catch(policy: .allErrors) { error in
+            error.isCancelled ? ex.fulfill() : XCTFail("Error: \(error)")
+        }.cancel()
+        
+        waitForExpectations(timeout: 1)
+    }
+
+#if swift(>=3.2)
+    func testCancelDecodable1() {
+        
+        func getFixture() -> CancellablePromise<Fixture> {
+            return Alamofire.request("http://example.com", method: .get).responseDecodableCC(queue: nil)
+        }
+        
+        let json: NSDictionary = ["key1": "value1", "key2": ["value2A", "value2B"]]
+        
+        OHHTTPStubs.stubRequests(passingTest: { $0.url!.host == "example.com" }) { _ in
+            return OHHTTPStubsResponse(jsonObject: json, statusCode: 200, headers: nil)
+        }
+        
+        let ex = expectation(description: "")
+        
+        getFixture().done { fixture in
+            XCTAssert(fixture.key1 == "value1", "Value1 found")
+            XCTFail("failed to cancel request")
+        }.catch(policy: .allErrors) { error in
+            error.isCancelled ? ex.fulfill() : XCTFail("Error: \(error)")
+        }.cancel()
+        
+        waitForExpectations(timeout: 1)
+    }
+    
+    func testCancelDecodable2() {
+        let json: NSDictionary = ["key1": "value1", "key2": ["value2A", "value2B"]]
+        
+        OHHTTPStubs.stubRequests(passingTest: { $0.url!.host == "example.com" }) { _ in
+            return OHHTTPStubsResponse(jsonObject: json, statusCode: 200, headers: nil)
+        }
+        
+        let ex = expectation(description: "")
+        
+        firstly {
+            Alamofire.request("http://example.com", method: .get).responseDecodableCC(Fixture.self)
+        }.done { fixture in
+            XCTAssert(fixture.key1 == "value1", "Value1 found")
+            XCTFail("failed to cancel request")
+        }.catch(policy: .allErrors) { error in
+            error.isCancelled ? ex.fulfill() : XCTFail("Error: \(error)")
+        }.cancel()
+        
+        waitForExpectations(timeout: 1)
+        
+    }
+    #endif
 }
