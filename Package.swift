1e35df75f1f5f6eb9f09c375088294b58180cf66
Radu Ursache <3800336+rursache@users.noreply.github.com>
2020-09-07 21:01:33 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,5 +6,11 @@ let package = Package(
         .Package(url: "https://github.com/mxcl/PromiseKit.git", majorVersion: 6),
         .Package(url: "https://github.com/Alamofire/Alamofire.git", majorVersion: 5)
     ],
+    platforms: [
+        .macOS(.v10_12),
+        .iOS(.v10),
+        .tvOS(.v10),
+        .watchOS(.v3)
+    ],
     exclude: ["Tests"]
 )
