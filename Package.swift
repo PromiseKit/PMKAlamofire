9ee64f7c05d8846428460fbbc6eef3ff049e6afb
Radu Ursache <3800336+rursache@users.noreply.github.com>
2020-09-07 21:14:30 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,14 +2,9 @@
 import PackageDescription
 
 let package = Package(name: "PMKAlamofire",
-                      platforms: [.macOS(.v10_12),
-                                  .iOS(.v10),
-                                  .tvOS(.v10),
-                                  .watchOS(.v3)],
-                      products: [.library(name: "PMKAlamofire",
-                                          targets: ["PMKAlamofire"])],
-                      targets: [.target(name: "PMKAlamofire",
-                                        path: "Source"],
+                      platforms: [.macOS(.v10_12), .iOS(.v10), .tvOS(.v10), .watchOS(.v3)],
+                      products: [.library(name: "PMKAlamofire", targets: ["PMKAlamofire"])],
+                      targets: [.target(name: "PMKAlamofire", path: "Source")],
                       dependencies: [.Package(url: "https://github.com/mxcl/PromiseKit.git", majorVersion: 6),
                                      .Package(url: "https://github.com/Alamofire/Alamofire.git", majorVersion: 5)],
                       swiftLanguageVersions: [.v5],
