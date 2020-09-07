e278a3e2f42bdd5457167a1deb732a503c5edafd
Radu Ursache <3800336+rursache@users.noreply.github.com>
2020-09-07 21:09:25 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,16 +1,16 @@
+// swift-tools-version:5.1
 import PackageDescription
 
-let package = Package(
-    name: "PMKAlamofire",
-    dependencies: [
-        .Package(url: "https://github.com/mxcl/PromiseKit.git", majorVersion: 6),
-        .Package(url: "https://github.com/Alamofire/Alamofire.git", majorVersion: 5)
-    ],
-    platforms: [
-        .macOS(.v10_12),
-        .iOS(.v10),
-        .tvOS(.v10),
-        .watchOS(.v3)
-    ],
-    exclude: ["Tests"]
-)
+let package = Package(name: "PMKAlamofire",
+                      platforms: [.macOS(.v10_12),
+                                  .iOS(.v10),
+                                  .tvOS(.v10),
+                                  .watchOS(.v3)],
+                      products: [.library(name: "PMKAlamofire",
+                                          targets: ["PMKAlamofire"])],
+                      targets: [.target(name: "PMKAlamofire",
+                                        path: "Source"],
+                      dependencies: [.Package(url: "https://github.com/mxcl/PromiseKit.git", majorVersion: 6),
+                                     .Package(url: "https://github.com/Alamofire/Alamofire.git", majorVersion: 5)],
+                      swiftLanguageVersions: [.v5],
+                      exclude: ["Tests"])
